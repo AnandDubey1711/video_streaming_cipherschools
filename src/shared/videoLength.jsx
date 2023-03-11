0f12a4187fc423a43ab970e689c5f2c7bd8f2c5e
Anand Dubey <anand.kumar.dubey.cse@adgitmdelhi.ac.in>
2023-03-11 23:10:43 +0530

    commit

--- src/shared/videoLength.jsx
+++ src/shared/videoLength.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 
-const videoLength = () => {
+const videoLength = ({time}) => {
+  const videoLlengthInSeconds = moment()
+  ?.startOf("day")
+  ?.seconds(time)
+  ?.format("H:mm:ss");
   return (
-    <div>videoLength</div>
+    <span className='absolute bottom-2 bg-black py-1 px-2 text-white text-xs rounded-md'>
+      {videoLlengthInSeconds}
+    </span>
   )
 }
 

cdc9d54fdc30bfb7657ec9db43c44a2a15fd978b
Anand Dubey <anand.kumar.dubey.cse@adgitmdelhi.ac.in>
2023-03-11 12:58:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/shared/loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loader = () => {
+  return (
+    <div>loader</div>
+  )
+}
+
+export default loader
